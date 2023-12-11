a907e0644cebcf78b93d327e9ace3133ca40ba6f
va-run-6626 <vbhutada143@gmail.com>
2023-12-11 12:26:20 +0530

    SCS length Done

--- src/DpOnStrings/Problems/Prog3_ShortestCommonSuperSequence.java
+++ src/DpOnStrings/Problems/Prog3_ShortestCommonSuperSequence.java
@@ -1,13 +1,60 @@
 package DpOnStrings.Problems;
 
+import java.util.Arrays;
+
 public class Prog3_ShortestCommonSuperSequence {
     public static void main(String[] args) {
          String a = "geek";
          String b = "eke";
-        System.out.println("Shortest Common Super Sequence : "+ helper(a,b));
+        System.out.println("Shortest Common Super Sequence using Dp memoization: "+ helper1(a,b));
+        System.out.println("Shortest Common Super Sequence using lcs : "+ helper2(a,b));
+        System.out.println("Shortest Common Super Sequence using Dp tabulation : "+ helper3(a,b));
     }
 
-    private static int helper(String a, String b) {
+    private static int helper3(String a, String b) {
+        int x = a.length();
+        int y = b.length();
+        int[][] dp = new int[x+1][y+1];
+        for(int i = 0;i <= x; i++){
+            for(int j = 0; j <=y; j++){
+                if(i == 0){
+                    dp[i][j] = j;
+                }else if(j == 0){
+                    dp[i][j] = i;
+                }else if(a.charAt(i-1) == b.charAt(j-1)){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }else{
+                    dp[i][j] = 1 + Math.min(dp[i-1][j] , dp[i][j-1]);
+                }
+            }
+        }
+        return dp[x][y];
+    }
+
+    private static int helper1(String a, String b) {
+        int x = a.length();
+        int y = b.length();
+        int[][] dp = new int[x+1][y+1];
+        for(int[] i : dp){
+            Arrays.fill(i,-1);
+        }
+        return solve(a,b,x,y,dp);
+    }
+
+    private static int solve(String a, String b, int x, int y,int[][] dp) {
+        if(x == 0)return y;
+        if(y == 0)return x;
+
+        if(dp[x][y] != -1)return dp[x][y];
+
+        if(a.charAt(x-1) == b.charAt(y-1)){
+            return dp[x][y] =  1 + solve(a,b,x-1,y-1,dp);
+        }else{
+            return dp[x][y] = 1 + Math.min(solve(a,b,x-1,y,dp),solve(a,b,x,y-1,dp));
+        }
+    }
+
+    private static int helper2(String a, String b) {
         int x = a.length();
         int y = b.length();
         int[][] dp = new int[x+1][y+1];
--- src/DpOnStrings/Problems/Prog4_PrintingShortestCommonSuperSequence.java
+++ src/DpOnStrings/Problems/Prog4_PrintingShortestCommonSuperSequence.java
@@ -1,7 +1,18 @@
 package DpOnStrings.Problems;
 
+import java.sql.SQLOutput;
+
 public class Prog4_PrintingShortestCommonSuperSequence {
     public static void main(String[] args) {
+        String a = "AGGTAB";
+        String b = "GXTXAYB";
 
+        System.out.println("Shortest Common supersequence : " + helper(a,b));
+    }
+
+    private static String helper(String a, String b) {
+        int x = a.length();
+        int y = b.length();
+        return null;
     }
 }
